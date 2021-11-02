948c8adee0748be09c955b6f4ac30346233490ac
Saboorek <44848625+saboorek@users.noreply.github.com>
2021-11-02 16:05:09 +0100

    Update admcmd.java

--- admcmd.java
+++ admcmd.java
@@ -21,5 +21,22 @@ public class AdmCmds implements CommandExecutor{
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String l, String[] args) {
+		if(cmd.getName().equalsIgonreCase("mspawn")){
+			if(sender.hasPermission("dwmc.admin.mspawn")){
+				if(args.Lenght !=1) {
+					retrun false;
+				}
+					List<String> str = new ArrayList<String>();
+					for(EntityType en : EntityType.values()){
+						str.add(en.name());
+					}
+				if(str.contains(args[0].toUpperCase())) {
+				if(sender instanceof Player) {
+					((Player)sender).getLocation().getWorld().spawnEntity(((Player)sender).getLocation(), EntityType.valueOf(args[0].toUpperCase()));
+					}
+				}
+			}
+		}
+		
 	}
 }
