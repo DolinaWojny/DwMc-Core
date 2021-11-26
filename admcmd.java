b1fd9ec2b697f7b95cf75ecdbb7b307eca13b84d
Saboorek <44848625+saboorek@users.noreply.github.com>
2021-11-26 11:50:28 +0100

    Update admcmd.java

--- admcmd.java
+++ admcmd.java
@@ -48,10 +48,41 @@ public class AdmCmds implements CommandExecutor{
 		}//cmd.getName().equalsIgnoreCase("heal")
 		if(cmd.getName().equalsIgnoreCase("gm") {
 			if(sender.hasPermission("dwmc.gamemode") {
-			
+				if(sender instanceof Player) {
+					sender.sendMessage("§6[INFO]§cTylko gracz może użyć tej komendy!");
+					return false;
+				}
+					if(args.lenght == 1) {
+						Player p = (Player) sender;
+						if(args[0].equals("a") {
+							p.setGameMode(GameMode.ADVENTURE);
+							sender.sendMessage("§6[INFO]§dUstawiono twoj gamemode na §3ADVENTURE");
+						}
+						if(args[0].equals("s") {
+							p.setGameMode(GameMode.SURVIVAL);
+							sender.sendMessage("§6[INFO]§dUstawiono twoj gamemode na §3SURVIVAL");
+						}
+						if(args[0].equals("c") {
+							p.setGameMode(GameMode.CREATIVE);
+							sender.sendMessage("§6[INFO]§dUstawiono twoj gamemode na §3CREATIVE");
+						}
+						if(args[0].equals("spec") {
+							p.setGameMode(GameMode.SPECTATOR);
+							sender.sendMessage("§6[INFO]§dUstawiono twoj gamemode na §3SPECTATOR");
+						}
+					} else {
+						sender.sendMessage("§dDostepne opcje:");
+						sender.sendMessage("§ca - ADVENTURE");
+						sender.sendMessage("§3s - SURVIVAL");
+						sender.sendMessage("§4c - CREATIVE");
+						sender.sendMessage("§8spec - SPECTATOR");
+					}
 			} else {
 				sender.sendMessage("Nie posiadasz uprawnień do wykonania tej komendy!");
 			}
 		} //cmd.getName().equalsIgnoreCase("gm")
+		if(cmd.getName().equalsIgnoreCase("") {
+		
+		}//cmd.getName().equalsIgnoreCase("")
 
 }//Public boolean
